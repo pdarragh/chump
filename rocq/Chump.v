f48f20766549a7af9b297c1ffeda1406e96b15bc
Justin Frank <justinpfrank@protonmail.com>
2024-04-23 17:10:48 -0400

    Added simple types

--- rocq/Chump.v
+++ rocq/Chump.v
@@ -20,7 +20,7 @@ Inductive pexp :=
 | Deref (l : pexp).
 
 Variant op1 := Not.
-Variant op2 := Plus | Minus | Mult | And | Or.
+Variant op2 := Plus | Minus | Mult | And | Or | Eq.
 
 Inductive exp :=
 | Int (i : Z) | Bool (b : bool)
@@ -43,24 +43,24 @@ Inductive c :=
 
 Definition addr := positive.
 
-Inductive env :=
-| Top (f : list addr)
-| Frame (f : list addr) (rst : env).
+Inductive env A :=
+| Top (f : list A)
+| Frame (f : list A) (rst : env A).
 
-Definition extend_frame fr v :=
+Definition extend_frame {A} (fr : env A) v :=
   match fr with
   | Top l => Top (v :: l)
   | Frame l E => Frame (v :: l) E
   end.
 
-Fixpoint lookup_frame (e : env) n : option (list addr) :=
+Fixpoint lookup_frame {A} (e : env A) n : option (list A) :=
   match n, e with
   | 0, Top f | 0, Frame f _ => Some f
   | S n, Frame _ rst => lookup_frame rst n
   | S _, Top _ => None
   end.
 
-Definition lookup_env (e : env) (v : var) : option addr :=
+Definition lookup_env {A} (e : env A) (v : var) : option A :=
   let '(i, j) := v in
   f <- lookup_frame e i ;;
   nth_error f j.
@@ -142,10 +142,12 @@ Definition eval_op2 o v1 v2 : option val :=
   | Mult, vInt i1, vInt i2 => ret (vInt (i1 * i2))
   | And, vBool b1, vBool b2 => ret (vBool (b1 && b2))
   | Or, vBool b1, vBool b2 => ret (vBool (b1 || b2))
+  | Eq, vInt i1, vInt i2 => ret (vBool (i1 =? i2)%Z)
+  | Eq, vPtr a1, vPtr a2 => ret (vBool (a1 =? a2)%positive)
   | _, _, _ => None
   end.
 
-Fixpoint eval (E : env) (St : store) (e : exp) : option val :=
+Fixpoint eval (E : env addr) (St : store) (e : exp) : option val :=
   match e with
   | Int i => ret (vInt i)
   | Bool b => ret (vBool b)
@@ -164,7 +166,7 @@ Fixpoint eval (E : env) (St : store) (e : exp) : option val :=
     eval_op2 o v1 v2
   end.
 
-Fixpoint do_break n E k : option (env * kont) :=
+Fixpoint do_break n E k : option (env addr * kont) :=
   match k, E with
   | kEndIf k, Frame _ E => do_break n E k
   | kSeq _ k, Frame _ E => do_break n E k
@@ -176,13 +178,13 @@ Fixpoint do_break n E k : option (env * kont) :=
   | kMt, _ | _, Top _ => None
   end.
 
-Definition CESK : Type := c * env * store * kont.
-Definition checkpoint : Type := env * kont * list (addr * val).
+Definition CESK : Type := c * env addr * store * kont.
+Definition checkpoint : Type := env addr * kont * list (addr * val).
 
 Variant event :=
 | ePure (st : CESK)
-| eInput (f : val -> CESK)
-| eOutput (o : val) (st : CESK)
+| eInput (f : Z -> CESK)
+| eOutput (o : Z) (st : CESK)
 | eCheckpoint (chkp : checkpoint) (st : CESK).
 
 
@@ -197,8 +199,8 @@ Definition next (st : CESK) : option event :=
                 store_add_next St v,
                 k))
   | LetInput body =>
-    ret (eInput (fun v => (body, extend_frame E (st_next St),
-                           store_add_next St v, k)))
+    ret (eInput (fun i => (body, extend_frame E (st_next St),
+                           store_add_next St (vInt i), k)))
   | Assign l e =>
     v <- eval E St e ;;
     ptr <- eval_pexp E St l ;;
@@ -225,7 +227,10 @@ Definition next (st : CESK) : option event :=
 
   | Output e =>
     v <- eval E St e ;;
-    ret (eOutput v (Noop, E, St, k))
+    match v with
+    | vInt i => ret (eOutput i (Noop, E, St, k))
+    | _ => None
+    end
 
   | Checkpoint ps =>
     ls <- mapT (eval_pexp E St) ps ;;
@@ -242,8 +247,8 @@ Definition do_reset (st : CESK) (p : checkpoint) : CESK :=
 (* IO log, allows for nondeterminism w/ still reasoning about what inputs were seen/are the same *)
 
 Variant io_event :=
-| io_in (v : val)
-| io_out (v : val)
+| io_in (i : Z)
+| io_out (i : Z)
 | io_check
 | io_reset.
 
@@ -255,12 +260,12 @@ Inductive step : state -> state -> Prop :=
 | step_pure : forall c1 c2 p io,
   next c1 = Some (ePure c2) ->
   step (c1, p, io) (c2, p, io)
-| step_input : forall c1 f v p io,
+| step_input : forall c1 f i p io,
   next c1 = Some (eInput f) ->
-  step (c1, p, io) (f v, p, io_in v :: io)
-| step_output : forall c1 c2 v p io,
-  next c1 = Some (eOutput v c2) ->
-  step (c1, p, io) (c2, p, io_out v :: io)
+  step (c1, p, io) (f i, p, io_in i :: io)
+| step_output : forall c1 c2 i p io,
+  next c1 = Some (eOutput i c2) ->
+  step (c1, p, io) (c2, p, io_out i :: io)
 | step_check : forall c1 c2 p p' io,
   next c1 = Some (eCheckpoint p c2) ->
   step (c1, p', io) (c2, p, io_check :: io)
@@ -281,3 +286,88 @@ with remove_reset l :=
   | io_check :: l' => io_check :: filter_log l'
   | _ :: l' => remove_reset l'
   end.
+
+Inductive ty := tInt | tBool | tPtr (t : ty).
+
+Inductive has_ty_pexp : env ty -> pexp -> ty -> Prop :=
+| has_ty_Var : forall G x t,
+  lookup_env G x = Some t ->
+  has_ty_pexp G (Var x) t
+| has_ty_Deref : forall G p t,
+  has_ty_pexp G p (tPtr t) ->
+  has_ty_pexp G (Deref p) t.
+
+Variant has_ty_op1 : op1 -> ty -> ty -> Prop :=
+| has_ty_Not : has_ty_op1 Not tBool tBool.
+
+Variant has_ty_op2 : op2 -> ty -> ty -> ty -> Prop :=
+| has_ty_Plus : has_ty_op2 Plus tInt tInt tInt
+| has_ty_Minus : has_ty_op2 Minus tInt tInt tInt
+| has_ty_Mult : has_ty_op2 Mult tInt tInt tInt
+| has_ty_And : has_ty_op2 And tBool tBool tBool
+| has_ty_Or : has_ty_op2 Or tBool tBool tBool
+| has_ty_Eq_Int : has_ty_op2 Eq tInt tInt tBool
+| has_ty_Eq_Ptr : forall t, has_ty_op2 Eq (tPtr t) (tPtr t) tBool.
+
+Inductive has_ty_exp : env ty -> exp -> ty -> Prop :=
+| has_ty_Int : forall G i, has_ty_exp G (Int i) tInt
+| has_ty_Bool : forall G b, has_ty_exp G (Bool b) tBool
+| has_ty_Ref : forall G p t,
+  has_ty_pexp G p t ->
+  has_ty_exp G (Ref p) (tPtr t)
+| has_ty_Load : forall G p t,
+  has_ty_pexp G p t ->
+  has_ty_exp G (Load p) t
+
+| has_ty_Op1 : forall G o e t1 t2,
+  has_ty_op1 o t1 t2 ->
+  has_ty_exp G e t1 ->
+  has_ty_exp G (Op1 o e) t2
+
+| has_ty_Op2 : forall G o e1 e2 t1 t2 t3,
+  has_ty_op2 o t1 t2 t3 ->
+  has_ty_exp G e1 t1 ->
+  has_ty_exp G e2 t2 ->
+  has_ty_exp G (Op2 o e1 e2) t3.
+
+Inductive has_ty_c : env ty -> nat -> c -> Prop :=
+| has_ty_Noop : forall G n, has_ty_c G n Noop
+| has_ty_Let : forall G n e t c,
+  has_ty_exp G e t ->
+  has_ty_c (extend_frame G t) n c ->
+  has_ty_c G n (Let e c)
+| has_ty_LetInput : forall G n c,
+  has_ty_c (extend_frame G tInt) n c ->
+  has_ty_c G n (LetInput c)
+| has_ty_Assign : forall G n p e t,
+  has_ty_pexp G p t ->
+  has_ty_exp G e t ->
+  has_ty_c G n (Assign p e)
+
+| has_ty_If : forall G n e c1 c2,
+  has_ty_exp G e tBool ->
+  has_ty_c (Frame [] G) n c1 ->
+  has_ty_c (Frame [] G) n c2 ->
+  has_ty_c G n (If e c1 c2)
+
+| has_ty_Loop : forall G n c,
+  has_ty_c (Frame [] G) (S n) c ->
+  has_ty_c G n (Loop c)
+
+| has_ty_Break : forall G n m,
+  m < n ->
+  has_ty_c G n (Break m)
+
+| has_ty_Output : forall G n e,
+  has_ty_exp G e tInt ->
+  has_ty_c G n (Output e)
+
+| has_ty_Seq : forall G n c1 c2,
+  has_ty_c (Frame [] G) n c1 ->
+  has_ty_c G n c2 ->
+  has_ty_c G n (Seq c1 c2)
+
+| has_ty_Checkpoint : forall G n ps ts,
+  Forall2 (has_ty_pexp G) ps ts ->
+  has_ty_c G n (Checkpoint ps).
+
